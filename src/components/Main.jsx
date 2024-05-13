0b49f1317400111860b992fc7bc448b3aaa2ffdf
jiho <jxxo.ih@gmail.com>
2024-05-13 17:59:41 +0900

    first push

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return ( 
+        <>
+         Blog
+         * google login
+         * 글목록
+         * 글작성, 수정, 삭제
+         + img, file upload
+         * 글작성시 공개, 비공개
+        </>
+     );
+}
+ 
+export default Main;
